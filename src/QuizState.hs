9ed00423e477df644a70a5b867fe534d2b53f3c4
Cristovão Morgado <cristovao.morgado@gmail.com>
2022-06-15 09:56:04 +0100

    code clean up

--- src/QuizState.hs
+++ src/QuizState.hs
@@ -1,36 +1,9 @@
 module QuizState(
-    mainS,answerSession,getSession
+    answerSession
 ) where
 
 import System.Random
 import Control.Monad.State
-
-mainS :: IO ()
-mainS = do
-    news <- execStateT (answerSession 1) []
-    guesses <- execStateT (answerSession 11) []
-    putStrLn $ "Success in " ++ (show guesses) ++ " tries."
-    x <- execStateT(getSession 1) []
-    putStrLn $ show x 
- --   answer <- getStdRandom (randomR (1,100)) -- think of a number
- --   putStrLn "I'm thinking of a number between 1 and 100, can you guess it?"
- --   guesses <- execStateT (guessSession answer) 0
- --   putStrLn $ "Success in " ++ (show guesses) ++ " tries."
-
-guessSession :: Int -> StateT Int IO ()
-guessSession answer = do
-    gs <- lift getLine    -- get guess from user
-    let g = read gs       -- convert to number
-    modify (+1)           -- increment number of guesses
-    case compare g answer of
-        LT -> do
-            lift $ putStrLn "Too low"
-            guessSession answer
-        GT -> do
-            lift $ putStrLn "Too high"
-            guessSession answer
-        EQ -> lift $ putStrLn "Got it!"
-
 
 
 answerSession :: Int -> StateT [Int] IO()
@@ -38,9 +11,3 @@ answerSession answer = do
     modify (++[answer])
 
 
-getSession :: Int -> StateT [Int] IO[Int]
-getSession x = do
-    get
-
-io :: IO a -> StateT [Integer] IO a
-io = liftIO
